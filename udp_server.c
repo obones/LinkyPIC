93c3764e3b3e989d543b9700a394af19fbd9b8f8
OBones <obones@free.fr>
2019-10-06 13:01:28 +0200

    Must answer requests properly

--- udp_server.c
+++ udp_server.c
@@ -2,30 +2,34 @@
 #include "mcc_generated_files/TCPIPLibrary/udpv4.h"
 #include "udp_server.h"
 #include "mcc_generated_files/TCPIPLibrary/ip_database.h"
+#include "mcc_generated_files/TCPIPLibrary/network.h"
 
-const char ExpectedID[] = "LinkyPIC";
+#define ExpectedID "LinkyPIC"
 
 typedef struct 
 {
-    char ID[sizeof(ExpectedID)];
+    char ID[sizeof(ExpectedID) - 1];
 } ReceivedPacket_t;
 
 void UDP_Server(int length)
 {
     ReceivedPacket_t ReceivedPacket;
     
-    UDP_ReadBlock(&ReceivedPacket, sizeof(ReceivedPacket));
-    
-    if (strcmp(ReceivedPacket.ID, ExpectedID) == 0)
+    //if (length == (int)sizeof(ReceivedPacket))
     {
-       error_msg ret = UDP_Start(ipv4Header.srcIpAddress, udpHeader.dstPort, udpHeader.srcPort);
-       
-       if(ret == SUCCESS)
-       {
-          UDP_Write32(ipdb_getAddress());
-           
-          UDP_Send();           
-       }        
+        UDP_ReadBlock(&ReceivedPacket, sizeof(ReceivedPacket));
+
+        //if (strcmp(ReceivedPacket.ID, ExpectedID) == 0)
+        {
+           error_msg ret = UDP_Start(ipv4Header.srcIpAddress, udpHeader.dstPort, ntohs(udpHeader.srcPort));
+
+           if(ret == SUCCESS)
+           {
+              UDP_Write32(ipdb_getAddress());
+
+              UDP_Send();           
+           }        
+        }
     }
 }
 
